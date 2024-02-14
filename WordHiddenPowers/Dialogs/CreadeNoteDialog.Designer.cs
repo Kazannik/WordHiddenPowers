85cec2597410485085912652d50485e147c77812
Михаил Казанник <kazannik.m@mail.ru>
2024-02-14 21:38:07 +0300

    fix commit

--- WordHiddenPowers/Dialogs/CreadeNoteDialog.Designer.cs
+++ WordHiddenPowers/Dialogs/CreadeNoteDialog.Designer.cs
@@ -43,10 +43,9 @@
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(557, 400);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Location = new System.Drawing.Point(418, 325);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(100, 27);
+            this.cancelButton.Size = new System.Drawing.Size(75, 22);
             this.cancelButton.TabIndex = 28;
             this.cancelButton.Text = "Отмена";
             // 
@@ -54,10 +53,9 @@
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Location = new System.Drawing.Point(448, 400);
-            this.okButton.Margin = new System.Windows.Forms.Padding(4);
+            this.okButton.Location = new System.Drawing.Point(336, 325);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(100, 27);
+            this.okButton.Size = new System.Drawing.Size(75, 22);
             this.okButton.TabIndex = 27;
             this.okButton.Text = "&ОК";
             // 
@@ -65,20 +63,19 @@
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.descriptionTextBox, 3);
             this.descriptionTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.descriptionTextBox.Location = new System.Drawing.Point(4, 322);
-            this.descriptionTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.descriptionTextBox.Location = new System.Drawing.Point(3, 261);
             this.descriptionTextBox.Multiline = true;
             this.descriptionTextBox.Name = "descriptionTextBox";
             this.descriptionTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.descriptionTextBox.Size = new System.Drawing.Size(653, 66);
+            this.descriptionTextBox.Size = new System.Drawing.Size(490, 54);
             this.descriptionTextBox.TabIndex = 31;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 109F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 109F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 82F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 82F));
             this.tableLayoutPanel1.Controls.Add(this.subcategoriesComboBox, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.descriptionTextBox, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.raitingBox, 0, 4);
@@ -87,16 +84,15 @@
             this.tableLayoutPanel1.Controls.Add(this.categoriesComboBox, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 74F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(661, 441);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(496, 358);
             this.tableLayoutPanel1.TabIndex = 35;
             // 
             // subcategoriesComboBox
@@ -108,21 +104,20 @@
             this.subcategoriesComboBox.FormattingEnabled = true;
             this.subcategoriesComboBox.IntegralHeight = false;
             this.subcategoriesComboBox.ItemHeight = 20;
-            this.subcategoriesComboBox.Location = new System.Drawing.Point(4, 36);
-            this.subcategoriesComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.subcategoriesComboBox.Location = new System.Drawing.Point(3, 29);
             this.subcategoriesComboBox.MaxDropDownItems = 20;
             this.subcategoriesComboBox.Name = "subcategoriesComboBox";
-            this.subcategoriesComboBox.Size = new System.Drawing.Size(435, 26);
+            this.subcategoriesComboBox.Size = new System.Drawing.Size(326, 26);
             this.subcategoriesComboBox.TabIndex = 34;
             this.subcategoriesComboBox.SelectedIndexChanged += new System.EventHandler(this.subcategoriesComboBox_SelectedIndexChanged);
             // 
             // raitingBox
             // 
             this.raitingBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.raitingBox.Location = new System.Drawing.Point(3, 394);
-            this.raitingBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.raitingBox.Location = new System.Drawing.Point(2, 320);
+            this.raitingBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.raitingBox.Name = "raitingBox";
-            this.raitingBox.Size = new System.Drawing.Size(437, 42);
+            this.raitingBox.Size = new System.Drawing.Size(328, 42);
             this.raitingBox.TabIndex = 32;
             this.raitingBox.Value = 0;
             // 
@@ -135,24 +130,22 @@
             this.categoriesComboBox.FormattingEnabled = true;
             this.categoriesComboBox.IntegralHeight = false;
             this.categoriesComboBox.ItemHeight = 20;
-            this.categoriesComboBox.Location = new System.Drawing.Point(4, 4);
-            this.categoriesComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.categoriesComboBox.Location = new System.Drawing.Point(3, 3);
             this.categoriesComboBox.MaxDropDownItems = 20;
             this.categoriesComboBox.Name = "categoriesComboBox";
-            this.categoriesComboBox.Size = new System.Drawing.Size(435, 26);
+            this.categoriesComboBox.Size = new System.Drawing.Size(326, 26);
             this.categoriesComboBox.TabIndex = 33;
             this.categoriesComboBox.SelectedIndexChanged += new System.EventHandler(this.categoriesComboBox_SelectedIndexChanged);
             // 
             // CreateNoteDialog
             // 
             this.AcceptButton = this.okButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(661, 441);
+            this.ClientSize = new System.Drawing.Size(496, 358);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CreateNoteDialog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Текстовые данные";
